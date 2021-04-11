851e3586f10ed690c774fd3c958b3647b9f78201
Arsh Noor Amin <ana002@bucknell.edu>
2021-04-10 20:28:06 -0400

    done

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, redirect
+from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+
+account_sid = "AC3f0ad074e24c894cbde99008b467d1ba"
+auth_token = "30b5003e14e356658633f53f7fd8c2c5"
+client = Client(account_sid, auth_token)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -14,18 +19,19 @@ def incoming_sms():
 
     # Determine the right reply for this message
     if body.lower() == 'hello':
-      msg_1 = client.messages \
-              .create(
-                    body="Arsh reached out!",
-                    from_='+15707019176',
-                    to='+14759991429'
-                )
-      msg_2 = client.messages \
-              .create(
-                    body="Arsh reached out!",
-                    from_='+15707019176',
-                    to='+19293889573'
-                )
+        resp.message("Your designated contacts have been reached!")
+        msg_1 = client.messages \
+                .create(
+                        body="Arsh reached out!",
+                        from_='+15707019176',
+                        to='+14759991429'
+                    )
+        msg_2 = client.messages \
+                .create(
+                        body="Arsh reached out!",
+                        from_='+15707019176',
+                        to='+19293889573'
+                    )
 
     return str(resp)
 
