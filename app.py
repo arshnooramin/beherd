c4bff29f1f97285002ad99133f8f9c41735758f8
Arsh Noor Amin <ana002@bucknell.edu>
2021-04-10 20:22:10 -0400

    done

--- app.py
+++ app.py
@@ -13,10 +13,19 @@ def incoming_sms():
     resp = MessagingResponse()
 
     # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
+    if body.lower() == 'hello':
+      msg_1 = client.messages \
+              .create(
+                    body="Arsh reached out!",
+                    from_='+15707019176',
+                    to='+14759991429'
+                )
+      msg_2 = client.messages \
+              .create(
+                    body="Arsh reached out!",
+                    from_='+15707019176',
+                    to='+19293889573'
+                )
 
     return str(resp)
 
