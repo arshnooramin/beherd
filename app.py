dee746a35171a04556d5c22dbeef8ef55f82d75d
Arsh Noor Amin <ana002@bucknell.edu>
2021-04-10 21:20:14 -0400

    done

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, redirect
+from flask import Flask, render_template, flash, request, redirect
 from twilio.rest import Client
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -7,6 +8,43 @@ app = Flask(__name__)
 account_sid = "AC3f0ad074e24c894cbde99008b467d1ba"
 auth_token = "30b5003e14e356658633f53f7fd8c2c5"
 client = Client(account_sid, auth_token)
+
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+name = "Undefined"
+msg = "You have been reached out!"
+ph_1 = "+14759991429"
+ph_2 = '+19293889573'
+
+class MainForm(Form):
+    name_f = StringField('Name')
+    msg_f = StringField('Message to Send')
+    ph_1_f = StringField('Designated Contact #1')
+    ph_2_f = StringField('Designated Contact #2')
+    submit = SubmitField('Save')
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = MainForm(request.form)
+
+    # print(form.errors)
+
+    if request.method == 'POST':
+        name=form.name_f.data
+        msg=form.msg_f.data
+        ph_1=form.ph_1_f.data
+        ph_2=form.ph_2_f.data
+
+        print(name,msg,ph_1,ph_2)
+
+    # if form.validate():
+    #     # Save the comment here.
+    #     print("Done")
+    # else:
+    #     flash('All the form fields are required. ')
+
+    return render_template('hello.html', form=form)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -22,15 +60,15 @@ def incoming_sms():
         resp.message("Your designated contacts have been reached!")
         msg_1 = client.messages \
                 .create(
-                        body="Arsh reached out!",
+                        body=msg,
                         from_='+15707019176',
-                        to='+14759991429'
+                        to=ph_1
                     )
         msg_2 = client.messages \
                 .create(
-                        body="Arsh reached out!",
+                        body=msg,
                         from_='+15707019176',
-                        to='+19293889573'
+                        to=ph_2
                     )
 
     return str(resp)
