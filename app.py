320b8587868b071f2faf00aa9492d8cf0d8bbed2
Arsh Noor Amin <ana002@bucknell.edu>
2021-04-11 17:02:37 -0400

    added db

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, flash, request, redirect, session
 from twilio.rest import Client
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from twilio.twiml.messaging_response import MessagingResponse
+from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 
@@ -11,6 +13,20 @@ client = Client(account_sid, auth_token)
 
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lsnznlhrwoydof:202f3c0a5ed662ce7672ca9d8f8fdc82f802fbb50b557fedb81ebf67faf9694a@ec2-35-174-35-242.compute-1.amazonaws.com:5432/dejij1924mip1"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Profile(db.Model):
+    __tablename__ = "profile"
+
+    code = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, code, name):
+        self.name = name
+        self.code = code
 
 class MainForm(Form):
     code_f = StringField('Codeword')
@@ -25,16 +41,22 @@ def get_data():
     form = MainForm(request.form)
 
     if request.method == 'POST':
-        session['code'] = request.form['code_f']
-        session['name'] = request.form['name_f']
-        session['msg'] = request.form['msg_f']
-        session['ph_1'] = request.form['ph_1_f']
-        session['ph_2'] = request.form['ph_2_f']
+        data = Profile(request.form['code_f'], request.form['name_f'])
+        db.session.add(data)
+        db.session.commit()
+        # session['code'] = request.form['code_f']
+        # session['name'] = request.form['name_f']
+        # session['msg'] = request.form['msg_f']
+        # session['ph_1'] = request.form['ph_1_f']
+        # session['ph_2'] = request.form['ph_2_f']
 
     return render_template('hello.html', form=form)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
+
+    print(Profile.query.all(), file=sys.stderr)
+
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = str(request.values.get('Body', None))
@@ -60,19 +82,19 @@ def incoming_sms():
 
     return str(resp)
 
-def send_msg():
-    msg_1 = client.messages \
-        .create(
-                body="[BeHerd]: " + session['msg'] + " -" + session['name'],
-                from_='+15707019176',
-                to=session['ph_1']
-            )
-    msg_2 = client.messages \
-        .create(
-                body="[BeHerd]: " + session['msg'] + " -" + session['name'],
-                from_='+15707019176',
-                to=session['ph_2']
-            )
+# def send_msg():
+#     msg_1 = client.messages \
+#         .create(
+#                 body="[BeHerd]: " + session['msg'] + " -" + session['name'],
+#                 from_='+15707019176',
+#                 to=session['ph_1']
+#             )
+#     msg_2 = client.messages \
+#         .create(
+#                 body="[BeHerd]: " + session['msg'] + " -" + session['name'],
+#                 from_='+15707019176',
+#                 to=session['ph_2']
+#             )
 
 if __name__ == "__main__":
     app.run(debug=True)
